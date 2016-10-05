01847c8843aed067b3e120fbc25d86792c690f64
Grace Patuwo <grace.patuwo@gmail.com>
2016-10-05 11:44:14 -0700

    completed adjacency list

--- 06_graphs_topographical_sort/lib/graph.rb
+++ 06_graphs_topographical_sort/lib/graph.rb
@@ -1,15 +1,29 @@
 class Vertex
-  def initialize(value)
+  attr_reader :in_edges, :out_edges, :value
 
+  def initialize(value)
+    @in_edges = []
+    @out_edges = []
+    @value = value
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_accessor :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    self.from_vertex = from_vertex
+    self.to_vertex = to_vertex
+    self.cost = cost
+
+    from_vertex.out_edges << self
+    to_vertex.in_edges << self
   end
 
   def destroy!
-
+    from_vertex.out_edges.delete(self)
+    to_vertex.in_edges.delete(self)
+    self.from_vertex = nil
+    self.to_vertex = nil
   end
 end
