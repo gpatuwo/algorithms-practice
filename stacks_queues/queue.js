0e04d072450b4d8718d508a7777d5fc282adc8fe
Grace Patuwo <grace.patuwo@gmail.com>
2017-04-01 21:12:12 -0700

    updated queue methods'

--- stacks_queues/queue.js
+++ stacks_queues/queue.js
@@ -1,13 +1,18 @@
 // FIFO, like array but reverse if taking out
 class Queue {
   constructor(){
-    this.size = 0;
     this.storage = [];
+    this.size = this.storage.length;
+    this.head = null;
+    this.tail = null;
   }
 
   enqueue(data){
     this.storage.push(data);
-    this.size++;
+    if (this.size === 0) {
+      this.head = data;
+    }
+    this.tail = data;
   }
 
   dequeue(){
@@ -15,7 +20,22 @@ class Queue {
       return null;
     } else {
       this.storage.shift();
+      this.head = this.storage[0];
     }
+  }
+
+  first(){
+    return this.head;
+  }
+
+  last(){
+    return this.tail;
+  }
+
+  max(){
+    // if can assume that all values are numbers
+    return Math.max(...this.storage);
+    // if not, then maybe sort (b-a) and return storage[0]
   }
 
 }
