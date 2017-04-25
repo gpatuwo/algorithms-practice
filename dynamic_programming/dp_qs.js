171b801ae69c18ee2488f9f20b3e7e94ead4c0f4
Grace Patuwo <grace.patuwo@gmail.com>
2017-04-25 13:50:06 -0700

    dp epi

--- dynamic_programming/dp_qs.js
+++ dynamic_programming/dp_qs.js
@@ -2,13 +2,12 @@
 
 // in O(n) time and space:
 // const util = require('util');
-
 let fibsHash = {};
 function recFibs(n){
   if (n <= 1) return n;
   else if (!fibsHash[n]) {
     // console.log(`n: ${n}`);
-    fibsHash[n] = fibs(n - 1) + fibs(n - 2);
+    fibsHash[n] = recFibs(n - 1) + recFibs(n - 2);
   }
   // console.log(util.inspect(fibsHash, null));
     // with fibsHash in scope above fct, can see in call stack that no repeats
@@ -30,3 +29,43 @@ function itFibs(n){
   return fibsNow;
 }
 // console.log(itFibs(3));
+
+
+
+// find max sum over all subarrays of a given array of ints PPPPP
+// ex [904, 40, 523, 12, -335, -385, -124, 481, -31]
+/*
+ex: [-1, 2, 3, -1, 9], [-3, -2, -1]
+let sum = arr[0]
+itr over arr,
+  add el to sum,
+  if bigger && arr[i] < sum,
+    then replace sum with nextSum
+    push sum into sumsArr
+  else if arr[i] > sum && nextSum, sum = arr[i]
+  else push sum into sumsArr, sum = arr[i]
+*/
+
+function runningMax(arr){
+  let minSum = 0; // -1,
+  let runningSum = 0; // -1, 1, 4, 3, 4, 13
+  let maxSum = 0; // 2, 5, 14
+
+  for (var i = 0; i < arr.length; i++) {
+    runningSum += arr[i];
+
+    if (runningSum < minSum) {
+      minSum = runningSum;
+    }
+
+    if (runningSum - minSum > maxSum) {
+      maxSum = runningSum - minSum;
+    }
+  }
+
+  return maxSum;
+}
+// console.log(runningMax([-1, 2, 3, -1, 1, 9]));
+  // should equal 14!!! 2, 3, -1, 1, 9 is the longest!
+// console.log(runningMax([2, 3, 9]));
+// console.log(runningMax([-3, -2, -1]));
